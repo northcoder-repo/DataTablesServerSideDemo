2c8b34f9e95deb76f7368fb041227865a1ec7ac2
northCoder <northcoder.root@gmail.com>
2023-07-29 14:17:10 -0400

    Add search column.

--- src/main/java/com/northcoder/demo/services/request/SqlQuery.java
+++ src/main/java/com/northcoder/demo/services/request/SqlQuery.java
@@ -5,8 +5,10 @@ import java.sql.Types;
 import java.util.ArrayList;
 import java.util.List;
 
+//
 // converts the DataTables request (ServerSideRequest) into a SQL prepared
 // statement string, together with parameters and parameter data types:
+//
 public class SqlQuery {
 
     private static final String DB = "datatables";
@@ -24,14 +26,16 @@ public class SqlQuery {
         String countClause = "select count(1) ";
         String selectClause = buildSelectClause(ssr);
         String fromClause = buildFromClause();
-        String whereClause = buildWhereClause(ssr);
+        String whereClauseGlob = buildGlobalWhereClause(ssr);
+        String whereClauseCols = buildColumnsWhereClause(ssr, whereClauseGlob);
         String orderByClause = buildOrderByClause(ssr);
         String limitOffsetClause = buildLimitOffsetClause(ssr);
 
         // how many rows would be selected if we did not use LIMIT and OFFSET:
-        this.countString = countClause + fromClause + whereClause;
+        this.countString = countClause + fromClause + whereClauseGlob + whereClauseCols;
         // only one page of DataTables results (using LIMIT and OFFSET):
-        this.sqlString = selectClause + fromClause + whereClause + orderByClause + limitOffsetClause;
+        this.sqlString = selectClause + fromClause + whereClauseGlob + whereClauseCols
+                + orderByClause + limitOffsetClause;
 
         this.params = paramsList.toArray(Object[]::new);
         this.paramTypes = paramTypesList.stream().mapToInt(i -> i).toArray();
@@ -70,7 +74,7 @@ public class SqlQuery {
         return sb.toString();
     }
 
-    private String buildWhereClause(ServerSideRequest ssr) {
+    private String buildGlobalWhereClause(ServerSideRequest ssr) {
         //
         // This logic assumes the database is MySQL and the table's collation is
         // utf8mb4_0900_ai_ci, where:
@@ -88,18 +92,61 @@ public class SqlQuery {
         if (globalSearch.isEmpty()) {
             return "";
         }
-        sb.append("where ");
+
+        // make sure at least one field is searchable:
+        long count = ssr.columns().stream().filter(col -> col.searchable()).count();
+        if (count == 0) {
+            return "";
+        }
+
+        sb.append("where (");
         for (Column column : ssr.columns()) {
             if (column.searchable()) {
                 sb.append(Employee.sqlColumn(column.data()))
                         // SQL: "like 'foo%'..."
                         // effectively, a "starts with" query:
                         .append(" like concat(?, '%') or ");
+                // prepared statement parameters and types:
                 paramsList.add(globalSearch);
                 paramTypesList.add(Types.VARCHAR);
             }
         }
-        sb.setLength(sb.length() - 3); // remove the final "or "
+        sb.setLength(sb.length() - 4); // remove the final " or "
+        return sb.append(") ").toString();
+    }
+
+    private String buildColumnsWhereClause(ServerSideRequest ssr, String whereClauseGlob) {
+        // just a demo using one column - the Office col, as a
+        // drop-down list, consisting of an exact office name
+
+        // first see if we have any column filter values:
+        long count = ssr.columns().stream().filter(
+                col -> col.search().value() != null && !col.search().value().isEmpty()
+        ).count();
+        if (count == 0) {
+            return "";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        if (whereClauseGlob.isBlank()) {
+            // start of the WHERE clause:
+            sb.append("where ");
+        } else {
+            // WHERE clause already started:
+            sb.append("and ");
+        }
+
+        for (Column col : ssr.columns()) {
+            if (col.searchable() && col.search().value() != null && !col.search().value().isBlank()) {
+                sb.append(Employee.sqlColumn(col.data()))
+                        // just our one demo drop-down column:
+                        .append(" = ? and ");
+                // prepared statement parameters and types:
+                paramsList.add(col.search().value());
+                paramTypesList.add(Types.VARCHAR);
+            }
+        }
+        sb.setLength(sb.length() - 4); // remove the final "and "
         return sb.toString();
     }
 
@@ -118,6 +165,7 @@ public class SqlQuery {
     }
 
     private String buildLimitOffsetClause(ServerSideRequest ssr) {
+        // get the currently requested page of data (for DataTables):
         return "limit " + ssr.length() + " offset " + ssr.start();
     }
 
